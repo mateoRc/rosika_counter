b733d301d2d50a4e83d12c66c5fb82c0d8fdc900
mateo <mmateo19992@gmail.com>
2024-04-18 01:34:58 +0200

    some stuff

--- rosika_2025/repository/timer_repo.py
+++ rosika_2025/repository/timer_repo.py
@@ -5,5 +5,5 @@ conn = sqlite3.connect('button_events.db')
 def store_btn_clicked_event(event):
     cursor = conn.cursor()
     cursor.execute("INSERT INTO events (button_id, timestamp) VALUES (?, ?)",
-                   (event.button_id, event.timestamp,))
+                   (event.button_id, event.timestamp))
     conn.commit()
--- rosika_2025/service/timer.py
+++ rosika_2025/service/timer.py
@@ -14,42 +14,51 @@ class Timer:
                     cls._instance._initialized = False
         return cls._instance
 
-    def __init__(self, seconds=30):
+    def __init__(self, seconds=5):
         self.start_time = seconds
         self.seconds = seconds
         self.current_button = None
         self.timer_thread = None
         self.timer_running = False
+        self.timer_completed = False
         if not self._initialized:
             self._initialized = True
             print("timer initialized...")
 
     def start_timer(self, button_id):
         print("start_timer " + str(button_id))
+        print(self.timer_running)
+        print(self.timer_completed)
         if self.current_button == button_id:
+            print("identical btn, ignored...")
             return
 
+        self.timer_completed = False
         if self.timer_running:
             print("Stopping previous timer...")
             self.stop_timer()
+        self.seconds = self.start_time
         self.timer_running = True
         self.timer_thread = threading.Thread(target=self._run_timer)
         self.timer_thread.daemon = True
         self.current_button = button_id
         print("timer started!")
-        self.timer_thread.start()
 
+        self.timer_thread.start()
 
     def stop_timer(self):
         if self.timer_thread and self.timer_thread.is_alive():
             self.timer_running = False
             self.timer_thread.join()
-            self.seconds = self.start_time
 
     def _run_timer(self):
         while self.seconds > 0 and self.timer_running:
             print(self.seconds)
             time.sleep(1)
             self.seconds -= 1
-        print("Timer finished!")
         self.timer_running = False
+        if self.seconds == 0:
+            self.timer_completed = True
+        print(self.timer_running)
+        print(self.timer_completed)
+        print("Timer finished!")
